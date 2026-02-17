030a6be0b0cfe37650859e2ac06da1f6d57c7c9a
ikkei1616 <is0727xe@ed.ritumei.ac.jp>
2026-02-17 16:50:57 +0900

    add:サインアップ機能の実装

---
+++ app/(auth)/signUp/actions.ts
@@ -0,0 +1,30 @@
+"use server"
+import { createClient } from '@/lib/supabase/server' // サーバー用クライアント
+import { redirect } from 'next/navigation'
+
+export async function signUp(formData:FormData) {
+  const supabase = await createClient();
+
+  const email = formData.get('email')
+  const password = formData.get('password')
+
+  // バリデーション
+  if (!email || !password || typeof email !== 'string' || typeof password !== 'string') {
+    redirect("/error?message=メールアドレスとパスワードを入力してください")
+  }
+
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/confirm`
+    }
+  })
+
+  if (error) {
+    redirect("/error")
+  }
+
+  // 成功時のリダイレクト
+  redirect("/")
+}
