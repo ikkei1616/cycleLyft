58c9bfb777f8f5e258e072312f5e183cc8fafb30
ikkei1616 <is0727xe@ed.ritumei.ac.jp>
2026-02-17 16:48:14 +0900

    add: proxyの修正

--- lib/supabase/proxy.ts
+++ lib/supabase/proxy.ts
@@ -39,12 +39,13 @@ export async function updateSession(request: NextRequest) {
 
   if (
     !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
+    !request.nextUrl.pathname.startsWith('/signIn') &&
+    !request.nextUrl.pathname.startsWith('/signUp') &&
     !request.nextUrl.pathname.startsWith('/auth')
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
-    url.pathname = '/login'
+    url.pathname = '/signIn'
     return NextResponse.redirect(url)
   }
 
